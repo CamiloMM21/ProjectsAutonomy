6224a5efbff9df82ccfdf438255c6f8926d2699e
unknown <jcmm1519@gmail.com>
2023-01-08 15:40:47 -0500

    first-commit

--- tailwindcss/src/components/Search.jsx
+++ tailwindcss/src/components/Search.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Search() {
   return (
-    <div className=" flex justify-center left-72 flex justify-start  ">
+    <div className="flex justify-center left-72 flex justify-start max-sm:pl-10 max-sm:pr-10 ">
         <div className='flex justify-center order-1  bg-white border border-black cursor-pointer'>
      <svg className=" hover:text-black text-slate-500 h-5 w-5" viewBox="0 0 20 20 " fill="currentColor">
         <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
