361a394993e5c1c2134254c79caa9c3c116baa45
CamiloMM21 <121126131+CamiloMM21@users.noreply.github.com>
2023-02-13 17:34:23 -0500

    Delete burguer directory

--- burguer/src/view/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function About() {
-  return (
-    <div><h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel iure, aliquam tempore tenetur consequuntur praesentium et ullam accusantium, aspernatur ea officia nesciunt earum voluptates suscipit rem sed, asperiores ut.</h1></div>
-  )
-}
-
-export default About
